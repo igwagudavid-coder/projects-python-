08ce1612d107f7d9cdd3fa64cb4d600476f88a8a
Igwagu David <igwagudavid@gmail.com>
2026-07-16 23:46:50 +0100

    Changed logger to be called from main.

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,16 +1,21 @@
 import logging
-from pathlib import Path
 from config.settings import BASE_DIR
-LOG_DIR = BASE_DIR / "logs"
-LOG_DIR.mkdir(exist_ok= True)
-LOG_FILE = LOG_DIR / "weather prediction.log"
-logging.basicConfig(
-    level = logging.INFO,
-    format = "%(asctime)s | %(levelname)s | %(message)s",
-    handlers = [
-        logging.FileHandler(LOG_FILE),
-        logging.StreamHandler()
-    ]
 
-)
-logger = logging.getLogger(__name__)
+LOG_DIR = BASE_DIR / "logs"
+LOG_FILE = LOG_DIR / "weather_prediction.log"
+
+def setup_logger():
+    logger = logging.getLogger("weather_prediction")
+    if logger.handlers:
+        return logger
+
+    LOG_DIR.mkdir(exist_ok =True)
+    logging.basicConfig(
+        level = logging.INFO,
+        format = "%(asctime)s | %(levelname)s | %(message)s",
+        handlers = [
+            logging.FileHandler(LOG_FILE),
+            logging.StreamHandler()
+        ]
+    )
+    return logging.getLogger("weather_prediction")
